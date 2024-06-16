dad91fb6f585025fb23d11f7ad8d93668369c54d
jason <2650498254@qq.com>
2024-06-16 21:50:32 +0800

    update 更新用户更新信息所需字段

--- joyoj-backend/src/main/java/com/ojason/joyoj/model/dto/user/UserUpdateMyRequest.java
+++ joyoj-backend/src/main/java/com/ojason/joyoj/model/dto/user/UserUpdateMyRequest.java
@@ -6,27 +6,29 @@ import java.io.Serializable;
 
 /**
  * 用户更新个人信息请求
- *
- * @author <a href="https://github.com/liyupi">程序员鱼皮</a>
- * @from <a href="https://yupi.icu">编程导航知识星球</a>
  */
 @Data
 public class UserUpdateMyRequest implements Serializable {
 
+    private static final long serialVersionUID = 1L;
     /**
      * 用户昵称
      */
     private String userName;
-
     /**
      * 用户头像
      */
     private String userAvatar;
-
     /**
      * 简介
      */
     private String userProfile;
-
-    private static final long serialVersionUID = 1L;
+    /**
+     * 用户性别
+     */
+    private Integer userSex;
+    /**
+     * 邮箱
+     */
+    private String userEmail;
 }
