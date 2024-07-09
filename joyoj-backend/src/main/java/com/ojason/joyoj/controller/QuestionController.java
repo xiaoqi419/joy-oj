12f40286ce611e142356a555aa2165f0abd235bc
jason <2650498254@qq.com>
2024-07-09 12:46:21 +0800

    update 更新题目管理接口

--- joyoj-backend/src/main/java/com/ojason/joyoj/controller/QuestionController.java
+++ joyoj-backend/src/main/java/com/ojason/joyoj/controller/QuestionController.java
@@ -143,6 +143,29 @@ public class QuestionController {
      * @param id
      * @return
      */
+    @GetMapping("/get")
+    public BaseResponse<Question> getQuestionById(long id, HttpServletRequest request) {
+        if (id <= 0) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        Question question = questionService.getById(id);
+        if (question == null) {
+            throw new BusinessException(ErrorCode.NOT_FOUND_ERROR);
+        }
+        User loginUser = userService.getLoginUser();
+        // 仅管理员和本人可查看
+        if (!question.getUserId().equals(loginUser.getId()) && !userService.isAdmin(loginUser)) {
+            throw new BusinessException(ErrorCode.NO_AUTH_ERROR);
+        }
+        return ResultUtils.success(question);
+    }
+
+    /**
+     * 根据 id 获取包装类（脱敏）
+     *
+     * @param id
+     * @return
+     */
     @GetMapping("/get/vo")
     public BaseResponse<QuestionVO> getQuestionVOById(long id, HttpServletRequest request) {
         if (id <= 0) {
