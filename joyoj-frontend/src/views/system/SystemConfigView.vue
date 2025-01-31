41733cb7d10fb59b6b51c957abfb9b7340adbb5d
JASON <pekilovex@gmail.com>
2025-02-01 01:19:32 +0800

    style: 修改配置界面样式

--- joyoj-frontend/src/views/system/SystemConfigView.vue
+++ joyoj-frontend/src/views/system/SystemConfigView.vue
@@ -1,35 +1,66 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted, ref, watch } from "vue";
+import { useRoute } from "vue-router";
+
+const isShowIntro = ref(true);
+const route = useRoute();
+const selectedKeys = ref([route.path]); // 初始化为当前路径
+// 页面加载时,从localStorage中获取isShowIntro
+onMounted(() => {
+  const storedIsShowIntro = localStorage.getItem("isShowIntro");
+  if (storedIsShowIntro) {
+    isShowIntro.value = storedIsShowIntro === "true";
+  }
+  watch(
+    () => route.path,
+    () => {
+      isShowIntro.value = route.path === "/system/config";
+    },
+    { immediate: true }
+  );
+  // 将isShowIntro存入localStorage
+  localStorage.setItem("isShowIntro", isShowIntro.value.toString());
+  // 监听路由变化
+  watch(
+    () => route.path,
+    newPath => {
+      selectedKeys.value = [newPath];
+    }
+  );
+});
+console.log(selectedKeys.value);
+</script>
 
 <template>
   <div id="systemConfigView">
     <a-card
-      :style="{ width: '1450px', marginBottom: '20px', minHeight: '600px' }"
+      :style="{
+        width: '1450px',
+        marginBottom: '20px',
+        minHeight: '600px',
+        height: 'auto'
+      }"
     >
       <div class="main-container">
         <a-layout style="height: 400px">
           <a-layout>
-            <a-layout-sider
-              breakpoint="xl"
-              :width="220"
-              collapsible
-              :collapsed="collapsed"
-              @collapse="onCollapse"
-            >
+            <a-layout-sider breakpoint="xl" :width="220" collapsible>
               <a-menu
                 :style="{ width: '200px', height: '100%' }"
-                :default-open-keys="['0']"
                 breakpoint="xl"
+                :selected-keys="selectedKeys"
+                :default-open-keys="['0']"
               >
                 <a-sub-menu key="0">
                   <template #icon>
                     <joy-svg-icon icon="house-gear" class="mr-1" />
                   </template>
-                  <template #title>基本配置</template>
+                  <template #title>系统配置</template>
                   <router-link to="/system/config/announce-settings">
-                    <a-menu-item key="0_0">
+                    <a-menu-item key="/system/config/announce-settings">
                       <a-space>
                         <joy-svg-icon icon="config" class="mr-1" />
-                        公告设置
+                        基础设置
                       </a-space>
                     </a-menu-item>
                   </router-link>
@@ -48,8 +79,34 @@
                     hoverable
                   >
                     <!-- 未选中配置菜单时显示的 -->
-
-                    <router-view></router-view>
+                    <div v-if="isShowIntro">
+                      <a-typography :style="{ marginTop: '-40px' }">
+                        <a-typography-title> 系统配置</a-typography-title>
+                        <a-typography-paragraph blockquote>
+                          <p :style="{ fontSize: '15px' }">
+                            在系统配置中可以进行系统的各种基础配置和网站信息设置，包括
+                            <a-typography-text type="primary">
+                              网站标题
+                            </a-typography-text>
+                            、
+                            <a-typography-text type="primary">
+                              网站描述
+                            </a-typography-text>
+                            、
+                            <a-typography-text type="primary">
+                              LOGO
+                            </a-typography-text>
+                            等。
+                          </p>
+                        </a-typography-paragraph>
+                        <a-typography-paragraph>
+                          <p :style="{ fontSize: '17px' }">
+                            点击左侧菜单进行配置。
+                          </p>
+                        </a-typography-paragraph>
+                      </a-typography>
+                    </div>
+                    <router-view v-else></router-view>
                   </a-card>
                 </a-watermark>
               </div>
